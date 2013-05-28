sudar/posts-by-tag

commit c68c22d05710b209f19f151b47de38f833aacd74

Added code to migrate custom post field key

posts-by-tag.php
@@ -6,7 +6,7 @@ Description: Provide sidebar widgets that can be used to display posts from a se
 Author: Sudar
 Donate Link: http://sudarmuthu.com/if-you-wanna-thank-me
 License: GPL
-Version: 2.7.4
+Version: 2.8
 Author URI: http://sudarmuthu.com/
 Text Domain: posts-by-tag
 
@@ -95,6 +95,10 @@ class PostsByTag {
     // boolean fields that needs to be validated
     private $boolean_fields = array( 'exclude', 'exclude_current_post', 'excerpt', 'content', 'thumbnail', 'author', 'date', 'tag_links');
 
+    // constants 
+    const CUSTOM_POST_FIELD_OLD = 'posts_by_tag_page_fields'; // till v 2.7.4
+    const CUSTOM_POST_FIELD = '_posts_by_tag_page_fields';
+
     /**
      * Initalize the plugin by registering the hooks
      */
@@ -133,6 +137,7 @@ class PostsByTag {
     function add_script_config() {
         // Add script only to Widgets page
         if ($this->is_on_plugin_page()) {
+            //TODO: Move this to a seperate js file
 ?>
 
     <script type="text/javascript">
@@ -186,7 +191,8 @@ class PostsByTag {
         $widget_tags = '';
 
         if ($post_id > 0) {
-            $posts_by_tag_page_fields = get_post_meta($post_id, '_posts_by_tag_page_fields', TRUE);
+            $this->update_postmeta_key($post_id);
+            $posts_by_tag_page_fields = get_post_meta($post_id, self::CUSTOM_POST_FIELD, TRUE);
 
             if (isset($posts_by_tag_page_fields) && is_array($posts_by_tag_page_fields)) {
                 $widget_title = $posts_by_tag_page_fields['widget_title'];
@@ -250,7 +256,7 @@ class PostsByTag {
             $fields['widget_tags'] = '';
         }
 
-        update_post_meta($post_id, '_posts_by_tag_page_fields', $fields);
+        update_post_meta($post_id, self::CUSTOM_POST_FIELD, $fields);
 
     }
 
@@ -289,9 +295,21 @@ class PostsByTag {
         return $output;
     }
 
-    // PHP4 compatibility
-    function PostsByTag() {
-        $this->__construct();
+    /**
+     * Update postmeta key.
+     *
+     * Uptill v2.7.4 the Plugin was using a old postmeta key without '_'. 
+     * This function updates the postmeta key. Eventually this function will be removed.
+     *
+     * @return void
+     */
+    private function update_postmeta_key($post_id) {
+        $old_value = get_post_meta($post_id, self::CUSTOM_POST_FIELD_OLD, TRUE);
+
+        if (isset($old_value) && is_array($old_value)) {
+            update_post_meta($post_id, self::CUSTOM_POST_FIELD, $old_value);
+            delete_post_meta($post_id, self::CUSTOM_POST_FIELD_OLD);
+        }
     }
 }
 
@@ -358,7 +376,8 @@ class TagWidget extends WP_Widget {
             // get tags and title from page custom fields
 
             if ($post_id > 0) {
-                $posts_by_tag_page_fields = get_post_meta($post_id, '_posts_by_tag_page_fields', TRUE);
+                $this->update_postmeta_key($post_id);
+                $posts_by_tag_page_fields = get_post_meta($post_id, self::CUSTOM_POST_FIELD, TRUE);
 
                 if (isset($posts_by_tag_page_fields) && is_array($posts_by_tag_page_fields)) {
                     if ($posts_by_tag_page_fields['widget_title'] != '') {

commit 1673540ef019d982d7224f82c9988df969cd6fa0

Fixed the bug that in custom field handling that was introduced in It was introduced in c68c22d05710b209f19f151b47de38f833aacd74

posts-by-tag.php
@@ -806,7 +806,7 @@ function posts_by_tag( $tags = '', $options = array(), $exclude = FALSE, $excerp
  *         <bool> tag_links Whether to display tag links at the end
  *         <string> link_target the value to the target attribute of each links that needs to be added
  */
-function get_posts_by_tag( $tags = '', $options = array(), $exclude = FALSE, $excerpt = FALSE, $thumbnail = FALSE, $order_by = 'date', $order = 'desc', $author = FALSE, $date = FALSE, $content = FALSE, $exclude_current_post = TRUE, $link_target = '' ) {
+function get_posts_by_tag( $giventags = '', $options = array(), $exclude = FALSE, $excerpt = FALSE, $thumbnail = FALSE, $order_by = 'date', $order = 'desc', $author = FALSE, $date = FALSE, $content = FALSE, $exclude_current_post = TRUE, $link_target = '' ) {
     global $wp_query;
     global $post;
 
@@ -841,7 +841,7 @@ function get_posts_by_tag( $tags = '', $options = array(), $exclude = FALSE, $ex
 
     $tag_id_array = array();
     
-    if ($tags == '') {
+    if ( $giventags == '' ) {
         // if tags is empty then take from current posts
         if (is_single()) {
             $tag_array = wp_get_post_tags($current_post_id);
@@ -851,7 +851,7 @@ function get_posts_by_tag( $tags = '', $options = array(), $exclude = FALSE, $ex
         }
     } else {
         // Get array of post info.
-        $tag_array = explode(",", $tags);
+        $tag_array = explode( ",", $giventags );
 
         foreach ($tag_array as $tag) {
             $tag_id_array[] = pbt_get_tag_ID(trim($tag));